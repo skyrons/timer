74ba6f221381547fff3ddcc6da56df9de7ef0f11
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2025-01-10 17:14:13 -0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
@@ -11,6 +12,8 @@ export function App() {
         <Button/>
         <Button/>
         <Button/>
+
+        <GlobalStyle/>
     </ThemeProvider>
    
   )
