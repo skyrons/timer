1eabb7269a75cfcf9a1929fa54223c3508dd80bf
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2025-01-10 16:03:58 -0300

    Styled Components

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export type ButtonVariant = 'primary'| 'secondary' | 'success' | 'warning' | 'danger';
+
+interface ButtonContainerProps {
+    variant: ButtonVariant;
+}
+
+const buttonVariants = {
+    primary: 'purple',
+    secondary: 'orange',
+    danger: 'red',
+    success: 'green'
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+    width:100px;
+    height: 40px;
+
+    ${props => {
+        return `background-color: ${buttonVariants[props.variant]}`
+    }}
+`
+
+
