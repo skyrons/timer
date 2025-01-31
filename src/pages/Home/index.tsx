95c2364bbc930d71b463b1b9c96f41b98ec82f8d
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2025-01-31 03:39:55 -0300

    validation form - zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,37 @@
 import { Play } from "phosphor-react";
-import { 
-        CountdownContainer, 
-        FormContainer, 
-        HomeContainer, 
-        MinutesAmountInput, 
-        Separator, 
-        StartCoutdownButton, 
-        TaskInput } from "./styles";
-
 import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import { 
+    CountdownContainer, 
+    FormContainer, 
+    HomeContainer, 
+    MinutesAmountInput, 
+    Separator, 
+    StartCoutdownButton, 
+    TaskInput 
+} from "./styles";
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+        .number()
+        .min(5, 'Número de minutos abaixo da capacidade mínima.')
+        .max(60, 'Número de minutos acima da capacidade máxima.'),
+})
 
 export function Home (){
-    const { register, handleSubmit, watch} = useForm()
+    const { register, handleSubmit, watch} = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    })
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     function handleCreateNewCycle(data: any) {
         console.log(data)
     }
 
     const task = watch('task')
-    const isDisabled = !task
+    const isSubmitDisabled = !task
 
     return (
         <HomeContainer>
@@ -61,7 +73,7 @@ export function Home (){
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCoutdownButton disabled={isDisabled} >
+                <StartCoutdownButton disabled={isSubmitDisabled} >
                     <Play size={24}/>
                     Começar
                 </StartCoutdownButton >
